11a7bef8fb61413dd2452edc5dca1ddf5181e183
Sephens <adesephens@gmail.com>
2022-05-15 10:02:34 +0300

    The first did not run because of the compilation format, I correceted the compiled file

--- C_P_1/add.c
+++ C_P_1/add.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
-int main()
+int number1, number2, sum;
+int main(void)
 {
 	int number1, number2, sum;
-	printf("Enter two integers");
+	printf(" Enter two integers ");
 	scanf("%d %d", &number1, &number2);
-
 	sum = number1 + number2;
-	printf("%d + %d", number1, number2,sum);
+	printf("%d + %d is equal to: %d", number1, number2,sum);
 	return 0;
 }
